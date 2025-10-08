c015f1575ea024ef345913d7f4f70e677f60780c
angelopozo <apozo2027@gmail.com>
2025-10-08 15:14:23 +0200

    removed unused class

--- src/main/java/com/intermodular/intro_backend/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.intermodular.intro_backend;
-
-// Clase sin configuración de seguridad. Se mantiene el archivo para evitar referencias
-// pero no aporta beans ni anotaciones.
-public class SecurityConfig { }
